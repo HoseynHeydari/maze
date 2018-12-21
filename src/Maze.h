c8630c45e4451908dbb3d6cd3d31089bf578b14c
Hoseyn <hhma1371@gmail.com>
2018-12-21 23:41:05 +0330

    print_maze() corrected. print_matrix() added for development help.

--- src/Maze.h
+++ src/Maze.h
@@ -14,12 +14,31 @@ public:
 	{
 	}
 
+	inline void print_matrix()
+	{
+		uint8_t i = 1;
+		int j = 2;
+		while (i <= width * length)
+		{
+			if (i == 10)
+				--j;
+			if (i == 100)
+				--j;
+			for (int k = 0; k < j; ++k)
+				printf("0");
+			printf("%u ", i);
+			if (i%width == 0)
+				printf("\n");
+			++i;
+		}
+	}
+
 	inline void print_maze()
 	{
 		uint8_t i = 1;
 		uint8_t j = 0;
-		bool changed;
-		while (i < width * length)
+		bool changed = false;
+		while (i <= width * length)
 		{
 			changed = (i == field[j]);
 			if (changed)
@@ -30,16 +49,18 @@ public:
 			++i;
 		}
 	}
+
 private:
 	inline void print_element(bool change)
 	{
 		if (change)
 			element = 1 - element;
 		if (element == 0)
-			printf("#");
+			printf("#   ");
 		if (element == 1)
-			printf(" ");
+			printf("@   ");
 	}
+
 	int element = 0;
 	const uint8_t width;
 	const uint8_t length;
