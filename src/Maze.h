96fc70a5ba8c62d4a9401b74a9aa348c15905725
Hoseyn <hhma1371@gmail.com>
2019-01-18 14:29:31 +0330

    Add base example. Remove last way maze construction.

--- src/Maze.h
+++ src/Maze.h
@@ -7,64 +7,18 @@
 class Maze
 {
 public:
-	inline Maze(const uint8_t width, const uint8_t length, const std::vector<uint8_t> field)
+	inline Maze(const uint8_t width, const uint8_t length)
 	: width(width)
 	, length(length)
-	, field(field)
+	, ground(0)
 	{
-	}
-
-	inline void print_matrix()
-	{
-		uint8_t i = 1;
-		int j = 2;
-		while (i <= width * length)
-		{
-			if (i == 10)
-				--j;
-			if (i == 100)
-				--j;
-			for (int k = 0; k < j; ++k)
-				printf("0");
-			printf("%u ", i);
-			if (i%width == 0)
-				printf("\n");
-			++i;
-		}
-	}
-
-	inline void print_maze()
-	{
-		uint8_t i = 1;
-		uint8_t j = 0;
-		bool changed = false;
-		while (i <= width * length)
-		{
-			changed = (i == field[j]);
-			if (changed)
-				++j;
-			print_element(changed);
-			if (i%width == 0)
-				printf("\n");
-			++i;
-		}
 	}
 
 private:
-	inline void print_element(bool change)
-	{
-		if (change)
-			element = 1 - element;
-		if (element == 0)
-			printf("#   ");
-		if (element == 1)
-			printf("@   ");
-	}
-
 	int element = 0;
 	const uint8_t width;
 	const uint8_t length;
-	const std::vector<uint8_t> field;
+	const std::vector<uint8_t, uint8_t> ground;
 };
 
 #endif // MAZE_H_
--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,5 @@
 
 int main()
 {
-	const uint8_t x = 14;
-	const uint8_t y = 14;
-	const std::vector<uint8_t> state = {15, 17, 30, 32, 45, 47, 60};
-//	const std::vector<uint8_t> state = {5, 7, 10, 12, 15};
-	Maze maze(x, y, state);
-	maze.print_matrix();
-	maze.print_maze();
 	return 0;
 }
